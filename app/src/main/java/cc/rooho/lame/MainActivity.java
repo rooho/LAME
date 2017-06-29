802f9f948a5cbd036a27a2ff445d599c3b39c63f
cola <cola@coladeMacBook-Pro.local>
2017-06-29 18:25:08 +0800

    init

---
+++ app/src/main/java/cc/rooho/lame/MainActivity.java
@@ -0,0 +1,18 @@
+package cc.rooho.lame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author by cola 2017年06月29日 6:14 PM
+ * @description :
+ * @modify by modifier
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
